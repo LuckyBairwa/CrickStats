e8d64c422ced4e3cfde86238bfa48b3c139ba542
LUCKY BAIRWA <luckyshairwal@gmail.com>
2026-05-25 01:33:04 +0530

    fix app name

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 // src/api/axios.ts 😎🔥
 
 import axios from 'axios';
-// const BASE_URL = 'https://crickstatsbackend-wukt.onrender.com/api';
-const BASE_URL = 'http://10.107.66.248:5000/api';
+const BASE_URL = 'https://crickstatsbackend-wukt.onrender.com/api';
+// const BASE_URL = 'http://10.107.66.248:5000/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
